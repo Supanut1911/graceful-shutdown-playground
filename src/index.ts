e40bfe7d8afd5ce66c3c44738b8cfe3048aee817
Supanut (Nut) DevOps MyOrder <supanut.devops@myorder.ai>
2024-03-12 00:01:48 +0700

    prepare implement mongodb

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,5 @@ process.on('SIGINT', () => {
 let server = app.listen(3000, function () {
   console.log('Example app listening on port 3000, on process id =', process.pid)
 })
+
+//implement mongo
